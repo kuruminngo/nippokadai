616887713539966e3b29395d9762a042ab4dbf68
kruminngo <ad@localhost>
2024-07-02 01:07:01 +0900

    課題１

---
+++ src/main/java/com/techacademy/service/EmployeeService.java
@@ -0,0 +1,118 @@
+package com.techacademy.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.techacademy.constants.ErrorKinds;
+import com.techacademy.entity.Employee;
+import com.techacademy.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder) {
+        this.employeeRepository = employeeRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    // 従業員保存
+    @Transactional
+    public ErrorKinds save(Employee employee) {
+        ErrorKinds result = employeePasswordCheck(employee);
+        if (ErrorKinds.CHECK_OK != result) {
+            return result;
+        }
+        if (findByCode(employee.getCode()) != null) {
+            return ErrorKinds.DUPLICATE_ERROR;
+        }
+        employee.setDeleteFlg(false);
+        LocalDateTime now = LocalDateTime.now();
+        employee.setCreatedAt(now);
+        employee.setUpdatedAt(now);
+        employeeRepository.save(employee);
+        return ErrorKinds.SUCCESS;
+    }
+
+    // 従業員更新 /*追加*/
+    @Transactional
+    public ErrorKinds update(String code, Employee employee) {
+        Employee existingEmployee = findByCode(code);
+        if (existingEmployee == null) {
+            return ErrorKinds.NOT_FOUND;
+        }
+        if (!"".equals(employee.getPassword())) {
+            ErrorKinds result = employeePasswordCheck(employee);
+            if (ErrorKinds.CHECK_OK != result) {
+                return result;
+            }
+            existingEmployee.setPassword(passwordEncoder.encode(employee.getPassword()));
+        }
+        existingEmployee.setName(employee.getName());
+        existingEmployee.setRole(employee.getRole());
+        existingEmployee.setUpdatedAt(LocalDateTime.now());
+        employeeRepository.save(existingEmployee);
+        return ErrorKinds.SUCCESS;
+    }
+
+    // 従業員削除
+    @Transactional
+    public ErrorKinds delete(String code, UserDetail userDetail) {
+        if (code.equals(userDetail.getEmployee().getCode())) {
+            return ErrorKinds.LOGINCHECK_ERROR;
+        }
+        Employee employee = findByCode(code);
+        LocalDateTime now = LocalDateTime.now();
+        employee.setUpdatedAt(now);
+        employee.setDeleteFlg(true);
+        return ErrorKinds.SUCCESS;
+    }
+
+    // 従業員一覧表示処理
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    // 1件を検索
+    public Employee findByCode(String code) {
+        Optional<Employee> option = employeeRepository.findById(code);
+        return option.orElse(null);
+    }
+
+    // 従業員パスワードチェック
+    private ErrorKinds employeePasswordCheck(Employee employee) {
+        if (isHalfSizeCheckError(employee)) {
+            return ErrorKinds.HALFSIZE_ERROR;
+        }
+        if (isOutOfRangePassword(employee)) {
+            return ErrorKinds.RANGECHECK_ERROR;
+        }
+        employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+        return ErrorKinds.CHECK_OK;
+    }
+
+    // 従業員パスワードの半角英数字チェック処理
+    private boolean isHalfSizeCheckError(Employee employee) {
+        Pattern pattern = Pattern.compile("^[A-Za-z0-9]+$");
+        Matcher matcher = pattern.matcher(employee.getPassword());
+        return !matcher.matches();
+    }
+
+    // 従業員パスワードの8文字～16文字チェック処理
+    public boolean isOutOfRangePassword(Employee employee) {
+        int passwordLength = employee.getPassword().length();
+        return passwordLength < 8 || 16 < passwordLength;
+    }
+
+}
